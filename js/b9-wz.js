6fde8e79ca989afb5be38d1ff755c9890d9bb462
= <mark@togglebit.org>
2015-01-15 05:56:16 -0600

    fixed step logic to handle enter key and cleaner click logic

--- js/b9-wz.js
+++ js/b9-wz.js
@@ -21,57 +21,110 @@
     //module will just enclose the entire module in a local scoped variable which will be returned
     //for inclusion in the containing module
     var module = {
-        init: function () {
+        init: function (element, callback) {
             var nextLabel = "Next";
-            var backLabel = "Back";
+            var prevLabel = "Back";
             var finishLabel = "Finish";
-
-            var wizardBack = function (index) {
-                if (index > 0) {
-                    return '<li><a href="#b9-wz' + (index - 1) + '" class="b9-wz-link">' + backLabel + '</a></li>';
-                } else {
-                    return '';
-                }
-            }
-
-            var wizardNext = function (index, lastIndex) {
-                if (index === lastIndex - 1) {
-                    return '<li><a href="#b9-wz' + (index + 1) + '" class="b9-wz-link">' + finishLabel + '</a></li>';
-                } else if (index < lastIndex) {
-                    return '<li><a href="#b9-wz' + (index + 1) + '" class="b9-wz-link">' + nextLabel + '</a></li>';
-                } else {
-                    return ''
-                }
-            }
-
-            var idx = 0;
             var pageVisible = 'b9-wz-visible';
             var currentTarget = {};
-            var wizards = document.getElementsByClassName('b9-wz');
+
+            /**
+            * unique target id generator
+            */
+            var hash = function () {
+                   var id = []
+                    for (var i=0;i<24;i++) {
+                        id.push((Math.floor(Math.random()*16) % 16).toString(16))
+                    }
+                    return id.join('')
+            }
+
+            /**
+            * Create target id's for each section
+            */
+            var wizards = element.getElementsByClassName('b9-wz');
             var elements = Array.prototype.forEach.call(wizards, function (wizardElement, index, arr) {
-                wizardElement.setAttribute('id', 'b9-wz' + index);
+                hashval = hash()
+                wizardElement.setAttribute('id', 'b9-wz' + hashval);
                 if (index === 0) {
                     wizardElement.className = wizardElement.className + ' ' + pageVisible;
                     currentTarget = wizardElement;
                 }
-                var links = document.createElement('div');
-                var wizardLinks = '<ul>' + wizardBack(index) + wizardNext(index, arr.length - 1) + '</ul>';
-                links.innerHTML = wizardLinks;
-                links.className = links.className + ' b9-wz-links';
-                wizardElement.appendChild(links);
             });
+
+            /**
+            * Advance to the next wizard panel
+            */
+            function moveNext() {
+                currentTarget.className = currentTarget.className.replace(pageVisible, '') || currentTarget.className;
+                if (currentTarget.nextElementSibling !== null) {
+                   currentTarget = currentTarget.nextElementSibling;
+                   currentTarget.className = currentTarget.className + ' ' + pageVisible;
+                }
+                return currentTarget.nextElementSibling;
+            }
+
+            /**
+            * Move to previous wizard panel
+            */
+            function movePrev() {
+                currentTarget.className = currentTarget.className.replace(pageVisible, '') || currentTarget.className;
+                if (currentTarget.previousElementSibling !== null) {
+                   currentTarget = currentTarget.previousElementSibling;
+                   currentTarget.className = currentTarget.className + ' ' + pageVisible;
+                }
+                return currentTarget.previousElementSibling;
+            }
+
+            /**
+            * User clicks enter key.  Either advance to next panel, or submit final changes
+            */
+            element.onsubmit = function (event) {
+                if (currentTarget.nextElementSibling !== null) {
+                 if (!moveNext()) {
+                   callback('finish', currentTarget);
+                 } else {
+                   callback('next', currentTarget);
+                 }
+                }
+            }
 
             /**
              * Now get the links, and handle the click :target handling
              */
-            var wizards = document.getElementsByClassName('b9-wz-link');
             var elements = Array.prototype.forEach.call(wizards, function (wizardElement, index, arr) {
+                var next = '';
+                if (index < arr.length - 2) {
+                    next = '<li><a href="#' +  arr[index+1].getAttribute('id') + '" b9-wz-button="next" class="b9-wz-link">' + nextLabel + '</a></li>'
+                } else if (index < arr.length -1) {
+                    next = '<li><a href="#' +  arr[index+1].getAttribute('id') + '" b9-wz-button="finish" class="b9-wz-link">' + finishLabel + '</a></li>'; 
+                    wizardElement.className = wizardElement.className + ' b9-wz-finish';
+                }
+                var prev = '';
+                if (index > 0) {
+                    prev = '<li><a href="#' +  arr[index-1].getAttribute('id') + '" b9-wz-button="prev" class="b9-wz-link">' + prevLabel + '</a></li>';
+                }
+
+                var links = document.createElement('div');
+                var wizardLinks = '<ul>' + prev + next + '</ul>';
+                links.innerHTML = wizardLinks;
+                links.className = links.className + ' b9-wz-links';
+                wizardElement.appendChild(links);
                 wizardElement.onclick = function (event) {
-                    var targetId = event.srcElement.hash.substr(1);
-                    currentTarget.className = currentTarget.className.replace(pageVisible, '') || currentTarget.className;
-                    currentTarget = document.getElementById(targetId);
-                    currentTarget.className = currentTarget.className + ' ' + pageVisible;
+                    var task = event.target.getAttribute('b9-wz-button');
+                    switch (task) {
+                    case 'next':
+                    case 'finish':
+                        moveNext();
+                        callback(task, currentTarget);
+                        break;
+                    case 'prev':
+                        movePrev();
+                        callback(task, currentTarget);
+                        break;
+                    }
                     event.preventDefault();
+                    !element.validate || element.validate();
                 }
             });
         }
